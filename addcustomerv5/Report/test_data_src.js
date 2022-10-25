81c5fb9dc33366a2dc8ed75b2f759d94b451d91b
Vlad774 <111026962+Vlad774@users.noreply.github.com>
2022-10-25 10:54:02 -0700

    Add files via upload

---
+++ addcustomerv5/Report/test_data_src.js
@@ -0,0 +1,1 @@
+﻿var __testDataSource={items:[{dataFilePath:'Default.xlsx',dataFileFormat:'excel',dataFileFolder:'C:\\Users\\student\\Documents\\Unified Functional Testing\\AddCustomer_Vlad_V05\\Res4',namePairs:[]}]};
